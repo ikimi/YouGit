5ca8833f95da74fb465ca0ca5e33c04d5ea48f75
icys13 <jiajun.li13@gmail.com>
2012-03-30 23:45:03 +0800

    bug调试完成 next step ：文件缓存

--- shell/bare.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#完成项目的创建，即初始化项目
-
-cd /var/www/YouGit/repo/ 
-mkdir $1
-cd $1
-git init --bare
--- shell/push.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# 1.创建者的信息写入gitosis.config
-# 2.将对gitosis.conf的修改提交给server
-
-cd /var/www/YouGit/gitosis-admin/ 
-git add *
-git commit -m $1
-git push origin
